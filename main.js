4d5543066570fe2ca8ba0722d5b301a6fedfaf82
lalahabi <159227690+lalahabi@users.noreply.github.com>
2024-02-20 23:36:25 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const API_KEY = 'e069d8ca7aec4b55b3b8477a11e2ee2f'
 let news=[]
 const getLatestNews = async () =>{
-    const url = new URL(`https://jellytimes.netlify.app=${API_KEY}`
+    const url = new URL(`https://jellytimes.netlify.app=kr&apiKey=${API_KEY}`
     );
     const response = await fetch(url);
     const data = response.json();
